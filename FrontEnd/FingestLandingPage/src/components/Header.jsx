d909927394f316f4a718b5cd8b4798611e5705df
Winston Pichardo <winston1420@hotmail.com>
2024-12-12 12:24:44 -0400

    Made home page almost fully responsible, fixed styling

--- FrontEnd/FingestLandingPage/src/components/Header.jsx
+++ FrontEnd/FingestLandingPage/src/components/Header.jsx
@@ -6,47 +6,48 @@ import './../../css/header.css';
 function Header({ 
     onServiciosClick, 
     onNosotrosClick,
-    onAgendarCitaClick 
 }) {
     return (
         <header className="header">
-            <h1 className="header-title">
-            <Link to='/' className='nav-link'>
-                <img
-                    src={logoImage} // Usamos la importación aquí
-                    alt="Logo Fingest"
-                    className="logo"
-                    onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-                />
-            </Link>
-            </h1>
-            <nav className="nav">
-                <ul className="nav-list">
-                    <li className="nav-item">
-                        <Link to='/agendar-cita' className='nav-link'>
-                            Agendar Cita
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <a 
-                            href="#servicios" 
-                            className="nav-link"
-                            onClick={(e) => { e.preventDefault(); onServiciosClick(); }}
-                        >
-                            Servicios
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a 
-                            href="#nosotros" 
-                            className="nav-link"
-                            onClick={(e) => { e.preventDefault(); onNosotrosClick(); }}
-                        >
-                            Nosotros
-                        </a>
-                    </li>
-                </ul>
-            </nav>
+            <div className="header-contents">
+                <h1 className="header-title">
+                <Link to='/' className='nav-link'>
+                    <img
+                        src={logoImage} // Usamos la importación aquí
+                        alt="Logo Fingest"
+                        className="logo"
+                        onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+                    />
+                </Link>
+                </h1>
+                <nav className="nav">
+                    <ul className="nav-list">
+                        <li className="nav-item">
+                            <Link to='/agendar-cita' className='nav-link'>
+                                Agendar Cita
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <a 
+                                href="#servicios" 
+                                className="nav-link"
+                                onClick={(e) => { e.preventDefault(); onServiciosClick(); }}
+                            >
+                                Servicios
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a 
+                                href="#nosotros" 
+                                className="nav-link"
+                                onClick={(e) => { e.preventDefault(); onNosotrosClick(); }}
+                            >
+                                Nosotros
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 }
