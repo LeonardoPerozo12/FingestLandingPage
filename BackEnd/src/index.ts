9816205291d05801440eed6ceaccbaae46759f8e
Winston Pichardo <winston1420@hotmail.com>
2025-02-08 19:55:09 -0400

    Configured cors and changed .gitignore file for both projects

--- BackEnd/src/index.ts
+++ BackEnd/src/index.ts
@@ -1,10 +1,16 @@
-import express, { Router } from "express";
+import express from "express";
+import cors from "cors";
 import userRoutes from "./routes/userRoutes";
-import appointmentRoutes from "./routes/appointmentRoutes"
-import { send } from "vite";
+import appointmentRoutes from "./routes/appointmentRoutes";
 
 const app = express();
 const port = 8000;
+
+// CROSS ORIGIN RESOURCE SHARING configuration
+app.use(cors({ 
+    origin: "http://localhost:5173", // Permitir peticiones desde el frontend
+    credentials: true,
+}));
 
 app.use(express.json()); // Middleware to parse JSON requests
 app.use("/api", userRoutes);
@@ -13,8 +19,14 @@ app.use("/api", appointmentRoutes);
 app.get("/", (req, res) => {
     res.send("<h1>API de Fingest<h1>");
 });
+
+
 app.listen(port, () => {
     console.log(`Now listening on port http://localhost:${port}`);
 });
 
-
+// Log incoming requests
+app.use((req, res, next) => {
+    console.log(`[${req.method}] ${req.url} - ${new Date().toISOString()}`);
+    next();
+});
